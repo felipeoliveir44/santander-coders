b7b34c720e76024a16e7aa492816f7295e5c4847
felipeoliveir44 <felipeoliveiragg@outlook.com>
2023-09-15 11:42:18 -0300

    Commit

--- exercicios/src/grande_deposito/GrandeDeposito.java
+++ exercicios/src/grande_deposito/GrandeDeposito.java
@@ -1,5 +1,6 @@
 package grande_deposito;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class GrandeDeposito {
@@ -7,13 +8,15 @@ public class GrandeDeposito {
         Scanner scanner = new Scanner(System.in);
 
         double valor = scanner.nextDouble();
+        DecimalFormat df = new DecimalFormat("0.00");
 
         if (valor > 0) {
-            //TODO: Imprimir a mensagem de sucesso, formatando o saldo atual (vide Exemplos).
+            System.out.println("Deposito realizado com sucesso!");
+            System.out.println("Saldo atual: R$ " + df.format(valor));
         } else if (valor == 0) {
-            //TODO: Imprimir a mensagem de valor inválido.
+            System.out.println("Encerrando o programa...");
         } else {
-             //TODO: Imprimir a mensagem de encerrar o programa.
+             System.out.println("Valor invalido! Digite um valor maior que zero.");
         }
     }
 }	
